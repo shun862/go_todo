aebd37aa5f2dbfe40078c54a6b4b027d2ce33371
shun.sekiguchi <nuhs.ihcugikes@gmail.com>
2026-05-06 16:15:51 +0900

    use sqlite

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module todo-app
 
 go 1.25.6
+
+require github.com/mattn/go-sqlite3 v1.14.44 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
-	"slices"
-	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	basePattern string = "/"
 )
 
 type Todo struct {
@@ -13,11 +19,36 @@ type Todo struct {
 	Done bool
 }
 
+var db *sql.DB
+
 var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 var todos []Todo
 
+func initDB() {
+	var err error
+	db, err = sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTable := `
+		CREATE TABLE IF NOT EXISTS todos (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			text TEXT,
+			done INTEGER
+		);
+	`
+
+	_, err = db.Exec(createTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", indexHandler)
+	initDB()
+
+	http.HandleFunc(basePattern, indexHandler)
 	http.HandleFunc("/add", addHandler)
 	http.HandleFunc("/delete", deleteHandler)
 	http.HandleFunc("/toggle", toggleHandler)
@@ -25,42 +56,74 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	selectQuery := `
+		SELECT id, text, done FROM todos ORDER BY id DESC
+	`
+	rows, err := db.Query(selectQuery)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer rows.Close()
+
+	var todos []Todo
+	for rows.Next() {
+		var t Todo
+		var done int
+		rows.Scan(&t.Id, &t.Text, &done)
+		t.Done = done == 1
+		todos = append(todos, t)
+	}
+
 	tmpl.Execute(w, todos)
 }
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		task := r.FormValue("task")
-		todo := Todo{
-			Id:   len(todos) + 1,
-			Text: task,
+
+		insertQuery := `
+			INSERT INTO todos (text, done) VALUES (?, ?)
+		`
+		_, err := db.Exec(insertQuery, task, false)
+
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
 		}
-		todos = append(todos, todo)
 	}
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	http.Redirect(w, r, basePattern, http.StatusSeeOther)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		idStr := r.FormValue("id")
-		id, _ := strconv.Atoi(idStr)
-		todos = slices.DeleteFunc(todos, func(t Todo) bool {
-			return t.Id == id
-		})
+		id := r.FormValue("id")
+
+		deleteQuery := `
+			DELETE FROM todos WHERE id = ?
+		`
+		_, err := db.Exec(deleteQuery, id)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		}
 	}
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	http.Redirect(w, r, basePattern, http.StatusSeeOther)
 }
 
 func toggleHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		idStr := r.FormValue("toggle")
-		id, _ := strconv.Atoi(idStr)
-		for i, todo := range todos {
-			if todo.Id == id {
-				todos[i].Done = !todos[i].Done
-				break
-			}
+		id := r.FormValue("toggle")
+		changeQuery := `
+			UPDATE todos 
+				SET done = CASE WHEN done = 1 THEN 0 ELSE 1 END
+				WHERE id = ?
+		`
+		_, err := db.Exec(changeQuery, id)
+
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
 		}
 	}
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	http.Redirect(w, r, basePattern, http.StatusSeeOther)
 }
